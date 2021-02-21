104705077a6b02c602cf1054ff67b6381cf47b94
Евгений Кашицын <gekan73@mail.ru>
2021-02-21 21:39:38 +0400

    21.02.2021

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanTwo=(HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+
+        Cat beanCatOne=(Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatOne.getMess());
+
+        Cat beanCatTwo=(Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatTwo.getMess());
+
+        System.out.println(bean==beanTwo);
+        System.out.println(beanCatOne==beanCatTwo);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String mess;
+
+    public String getMess() {
+        return mess;
+    }
+
+    public void setMess(String mess) {
+        this.mess = mess;
+    }
+}
